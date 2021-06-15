37f5f29d72aa49397c0ac469f30d90ef942a37e3
Gabe Deml <gddeml@wpi.edu>
2021-06-15 21:51:21 +0000

    Well is seems to somewhat work

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 # from brain import Brain
+import random
+class Being:
+    weight = 0;
+    bias = 0;
+
+    def mutate(self):
+        if bool(random.getrandbits(1)):
+            self.weight += (random.random() - 0.5)
+        else:
+            self.bias += (random.random() - 0.5)
+
+    def test(self, input : int):
+        return (self.weight * input) + self.bias 
+
+
+goal = 1
+
 if __name__ == "__main__":
     print("Welcome to big inteligent machine brain")
+    # random.seed(1)
 
+    beings = []
     # This is a super simple implementation of the brain I just want to prove that is some what works
 
+    # Generate beings
     for i in range(0, 100):
-        
+        beings.append(Being())
+
+    bestBeing = beings[0]
+    bestBeingScore = 100000000000
+
+    # For x generations
+    for generation in range(0, 100):
+        # First mutate everything 
+        for b in beings:
+            b.mutate()
+        # Test to find the best of the beings
+        for b in beings:    
+            tmpScore = abs(b.test(1)) - goal 
+            if(tmpScore < bestBeingScore):
+                bestBeing = b
+                bestBeingScore = tmpScore
+
+    print(bestBeingScore)
+                        
+
+
 
     # neuron = Neuron();
